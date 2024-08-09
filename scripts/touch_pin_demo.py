62152ff7f94ad49269c4086232d1b710bf3cd4df
Garth Martinsen <garth.martinsen@gmail.com>
2024-08-09 08:34:28 -0700

    Used math.floor to truncate computed values to ints.

--- scripts/touch_pin_demo.py
+++ scripts/touch_pin_demo.py
@@ -1,10 +1,13 @@
 from machine import Pin, ADC
 import time
 import TouchPin
+''' This script reads the pin_to_test  up to the tp._cnt indicated. The user chooses at which cnts to touch the pin, starting after _cnt=25.
+The plotter should be enabled to show the touches. Any adc count which falls outside of bounds (lb,ub) is a TOUCH.'''
 
-
-tp = TouchPin.TouchPin(7)
-while tp._cnt < 200:
+pin_to_test = 7
+times_to_read = 200
+tp = TouchPin.TouchPin(pin_to_test)
+while tp._cnt < times_to_read :
     adc = tp.read()
     if type(adc) != int:
         continue
