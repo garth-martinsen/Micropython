7f8feeeeada7b0e7da69af40df38a74f6ea57c9f
Garth Martinsen <garth.martinsen@gmail.com>
2024-08-17 11:23:46 -0700

    Corrected Spacing in TouchPin.py

--- scripts/TouchPin.py
+++ scripts/TouchPin.py
@@ -29,7 +29,7 @@ class TouchPin:
         self._cnt =0
         self._smooth_sz=sb
         self._samples =[]
-        self._deviations = []
+        self._variants = []
         
     
     def read(self):
@@ -37,9 +37,24 @@ class TouchPin:
         adc = self._pin.read_u16()
         self.handle_sample(adc)
         
-        
+    def smooth(self, n, adc):
+        '''averages over the last n samples'''
+        self._samples.append(adc)
+        if len(self._samples) > n:
+            self._samples.pop(0)                                       #when list holds 1 more than _smooth_sz, remove the first element to keep the list size at _smooth_sz
+        mean = sum(self._samples)/len(self._samples)
+        # if less than 2 samples then just assign sd=250; guess!
+        sd=   250
+        self._variants.append((mean -adc)*(mean-adc))
+        if len(self._variants) > n:
+            self._variants.pop(0)                                       #when list holds 1 more than _smooth_sz, remove the first element to keep the list size at _smooth_sz
+        if len(self._variants) > 2:
+            sd = math.sqrt(sum(self._variants)/(len(self._variants) -1))
+                
+        return math.floor(mean), math.floor(sd)           # return as truncated ints because adc counts are ints.
+                
     def handle_sample(self, adc):
-        '''Samples are handled in one of  two states: {CALIBRATE, DETECT}. The state transistions from CALIBRATE-> DETECT when count >25.'''
+        '''Samples are handled in one of  two states: {CALIBRATE, DETECT}. The state transistions from CALIBRATE-> DETECT when count >_smooth_sz'''
         self._adc = adc
       
         if type(adc) != int:
@@ -52,48 +67,39 @@ class TouchPin:
             self._state = DETECT
             self.detect(adc)
       
-    def smoothBy(self, n):
-        if self._cnt > self._smooth_sz:
-            self._samples.pop(-(n+1))
-        return sum(self._samples)/len(self._samples)
-        
+ 
     def calibrate(self, adc):
-        ''' Calibration is done until sample size > 25, and also for NON-TOUCH, bounded measurements, after sample size > 25'''
+        ''' Calibration is complete when sample size > _smooth_sz , and also after _cnt > _smooth_sz,  for samples within bounds (ie: NON-TOUCH samples)  '''
         self._cnt +=1
+       
         if self._cnt >2:
-            mean= smooth_by(self._smooth_sz)
-            sd = math.sqrt(self._sum_variants/(self._cnt -1))
-            self._lb  = math.floor(mean - 0.9* sd)
-            self._ub = math.floor(mean + 0.9 * sd)
-            self._sum_samples += adc
-            self._sum_variants += math.floor((mean-adc)**2)
-         
+            mean, sd = self.smooth(self._smooth_sz, adc)
+            self._lb  = math.floor(mean - 3.5* sd)                                             #bounds: samples > abs(mean +- 3.5 sd) , will be branded as TOUCHes
+            self._ub = math.floor(mean + 3.5 * sd)
             print("id-cnt-adc-mean-lb-ub: ", self._id, sep, self._cnt, sep, self._adc,sep, mean, sep, self._lb, sep, self._ub)
 
        
     def detect(self, adc):
-        ''' bounds are at mean -2*sd and mean+2*sd. If bounds are exceeded,  it is a TOUCH. Observation shows that a 65535 or a zero could trigger
-a TOUCH so testing for anything outside of the bounds works well... No false TOUCHES'''
-        mean = math.floor(self._sum_samples/self._cnt)
+        ''' bounds are at mean -x*sd and mean+x*sd, where x is chosen by user from inspection (eg: 1.3).
+    If bounds are exceeded, it is a TOUCH. Observation shows that a 65535 or a zero could trigger
+        a TOUCH so testing for anything outside of the bounds works well... No false TOUCHES'''
+        mean = math.floor(sum(self._samples)/len(self._samples))
         if adc < self._lb or adc > self._ub:
             print("     Touch!:  id-cnt-adc-mean-lb-ub: ", self._id, sep, self._cnt, sep, self._adc, sep, mean, sep, self._lb, sep, self._ub)       #do not pass adc to calibrate(). It is an outlier
         else: 
             self.calibrate(adc)        # not a TOUCH, so use adc to improve estimation of thresh
- 
-              
-      
-           
-            
+        
     def createMeas(self, n_samples, low, high):
         '''This is for testing only. it will generate samples between low and high'''
         for i in range(n_samples):
             self.handle_sample( random.randrange(low*1000, high*1000)/10)
 
     def stats(self):
-        print("id: ", self._id)
+        ''' statistics are all truncated to ints as adc counts cannot be floats'''
+        print("pin: ", self._id)
         print("Count", self._cnt )
-        print("Mean: ", self._sum_samples /self._cnt)
-        print("Std Dev: ", math.sqrt(self._sum_variants/(self._cnt-1)))
+        print("Mean: ", math.floor(sum(self._samples) /len(self._samples)))
+        print("Std Dev: ", math.floor(math.sqrt(  sum(self._variants)/len(self._variants  ) )))
         print("LowerBound: ", self._lb)
         print("UpperBound: ", self._ub)
               
