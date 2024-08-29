55a253cb145822e498ada440ef25b6a3b6f3b585
Garth Martinsen <garth.martinsen@gmail.com>
2024-08-28 22:23:14 -0700

    Added more attributes to show_parameters() function.

--- scripts/My_INA219.py
+++ scripts/My_INA219.py
@@ -4,40 +4,54 @@
 
 #TODO: untangle all of the adafruit import and logging import   File "/lib/Adafruit_GPIO/__init__.py", line 1, in <module> ImportError: no module named '__future__' 
 import logging
-import ina219
 from machine import SoftI2C as SI2C
 from machine import Pin
 import errno
-from ina219 import INA219
+from INA219 import INA219
 
 def show_parameters(ina):
-    print("Measuring voltage, current, and power with INA219 ...")
+    print("Measuring voltage, current, and power with: ...")
+    print("Peripheral type: ", ina.__qualname__)
     print("ina219._shunt_ohms: ", ina._shunt_ohms)
     print("ina219._max_expected_amps: ", ina._max_expected_amps)
-    print("ina219.shunt_voltage: ", ina.shunt_voltage())
-    #print("ina219._voltage_range : ", ina._voltage_range )
-    print("ina219._gain: ", ina._gain)
+    print("INA219 Periferal address decimal: ", ina.address)
+    print("Registers: Config_REG:  /x{0},  Shunt_Reg /x0{1},  Voltage_REG  /x0{2},  Power_REG  /x0{3},  Current_REG  /x0{4},  Calibration_REG  /x0{5}".format(
+        ina.__REG_CONFIG, ina.__REG_SHUNTVOLTAGE, ina.__REG_BUSVOLTAGE, ina.__REG_POWER,
+        ina.__REG_CURRENT, ina.__REG_CALIBRATION))
+    print("Possible Bus Ranges: ", ina.__BUS_RANGE)
+    print("Using FSR voltage range: ",ina.__BUS_RANGE [0] )
+   
+   #print("compute current LSB: " ina._configure(ina.address, ,,)
+   # print("ina219.shunt_voltage: ", ina.shunt_voltage())
+   #print("ina219._voltage_range : ", ina._voltage_range )
+   #print("ina219._gain: ", ina._gain)
 
+''' configure(self, voltage_range=RANGE_32V, gain=GAIN_AUTO,
+                  bus_adc=ADC_12BIT, shunt_adc=ADC_12BIT):
+                  '''
     
     
-SCL=Pin(39)
-SDA=Pin(40)
+SCL=Pin(21)
+SDA=Pin(22)
 # i2c=I2C(0x40, *, SCL,SDA)
 i2c=SI2C(  SCL,SDA, freq=400000 )
 #i2c.start()
-#(self, shunt_ohms, max_expected_amps=None, busnum=None, address=__ADDRESS,   log_level=logging.DEBUG):
+# __init__( shunt_ohms, max_expected_amps=None, busnum=None, address=__ADDRESS,   log_level=logging.DEBUG):
 ina219 =  INA219(0.1, 3.2)
+          
+          
 ina219._i2c=i2c
 show_parameters(ina219)
 
 #print(help(i2c))
-print(" scan i2c devices in hex: ", i2c.scan())
+print(" scan i2c devices...: ", i2c.scan())
 #TODO: Find out why the esp32 cannot see the INA219 as device 0x40. ENODEV means it cannot see the device. https://stackoverflow.com/questions/64269494/micropython-oserror-errno-19-enodev
-#TODO: Find out if I need to initialize i2c with the id (0x40)
-#print("Trying to read 10 bytes from INA219:", i2c.readfrom(0x40, 10, True))
-print("Writing  to Config Register: : 0x1C5F")
+#TODO: Find out if I need to initialize i2c with the id (0x40). No it defaults to 0x40 = 64d
+print("Trying to read 10 bytes from INA219:", ina219._i2c.readfrom(0x40, 10, True))
 conf =b'1C5F'
-i2c.writeto_mem(0x40,0x00,conf)
+print("Writing  to Config Register:  ", conf)
+
+ina219._i2c.writeto_mem(0x40,0x00,conf)
 
 configreg = INA219._configuration_register(0x40,0x00)
 print("Config Register try to  read 2 bytes: ", i2c.readfrom_mem(0x40, 0x00, 2))
